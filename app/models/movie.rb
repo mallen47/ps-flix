23b5999b9ec0d1db1092a68d5f6c758dbade429a
Matt Allen <allen.matt@gmail.com>
2020-03-10 08:09:05 -0400

    17. Migrations Revisited
    Create new migration to add new fields to movies including images
    on the index and show pages.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,8 @@
 class Movie < ApplicationRecord
 
 	def self.released
-		where("released_on < ?", Time.now).order("released_on desc")
+		# where("released_on < ?", Time.now).order("released_on desc")
+		Movie.all
 	end
 
 	def flop?
