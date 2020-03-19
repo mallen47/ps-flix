6da8f3437b4a59ca0643441292caf861945a7305
Matt Allen <allen.matt@gmail.com>
2020-03-19 08:44:24 -0400

    22-12M-has-many
    Create the 'has many' side of the association on the Movies model
    so that a movie can have many reviews.
    
    Also, add validations on the review model to enforce data integrity.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :movie
+
+  validates :name, presence: true
+  validates :comment, length: { minimum: 4 }
+  STARS = (1..5).to_a
+  validates :stars, inclusion: {
+  	in: STARS,
+  	message: 'Must be between 1 and 5'
+  }
 end
