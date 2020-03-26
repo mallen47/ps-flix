ca66efa14c81eb26f62948262597b12e5e79acfa
Matt Allen <allen.matt@gmail.com>
2020-03-26 08:38:09 -0400

    26. User Account Model
    set up user resource, migration, validations, etc.

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false },
+                    allow_blank: true
+end
