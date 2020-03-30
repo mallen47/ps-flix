362206838be2b372512ee2ed19e7b7788b380649
Matt Allen <allen.matt@gmail.com>
2020-03-30 07:32:20 -0400

    30. Authentication
    Use Session object to handle user authentication and keep track of
    current user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,15 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		params[:email]
-		params[:password]
+		user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect_to user, notice: "Welcome back #{user.name}!"
+		else
+			flash.now[:alert] = "Invalid user/password combination"
+			render :new
+		end
+		
 	end
 
 	def destroy
